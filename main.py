102f49017479315bdbd8052e19fa02c86239fc79
cdspaceroot <cdspaceroot@gmail.com>
2022-08-27 14:41:58 +0300

    Добавить html основной страницы

--- main.py
+++ main.py
@@ -3,6 +3,21 @@ import datetime
 import bottle
 
 
+INDEX_HTML = """
+<link href="http://fonts.cdnfonts.com/css/gost-type-a" rel="stylesheet">
+
+<p style="
+        font-family:'GOST type A';
+        color:#ff0000;
+        font-size:80px;
+        font-weight:bold
+        "
+    >
+    Текущее время: <span id="current_time"></span>
+</p>
+"""
+
+
 @bottle.route("/current_time")
 def current_time():
     return str(datetime.datetime.now())
@@ -10,7 +25,7 @@ def current_time():
 
 @bottle.route("/")
 def index():
-    return ""
+    return INDEX_HTML
 
 
 bottle.run(host="localhost", port=8080)
